df0a8fa63c26808611ee9c3f4f3b1cea0ad219c2
Mohammad Umair Khan <mohammad.umair.k@gmail.com>
2016-04-22 21:50:46 +0500

    package updates

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 var express = require('express');
-var bodyParser = require('body-parser')
+var bodyParser = require('body-parser');
 
 var www = express.static('public');
 var api = require('./api/app.js');
 
 express()
-  .use(bodyParser.urlencoded())
+  .use(bodyParser.urlencoded({extended: true}))
   .use(bodyParser.json())
   .use('/api', api)
   .use('/', www)
